14b264294d473f955b1a4f5276b1928fefbcbead
hsnwf15 <hsnlwfa15@gmail.com>
2024-03-31 17:19:56 +0800

    Memanggil method dari Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("== Data Mahasiswa ====================");
+        System.out.println("NPM  : 2210010318");
+        System.out.println("Nama : Muhammad Husnu Elwafa");
+        System.out.println("======================================");
+    }
+}
